5e30f1ae359fc86e166899a32b1d1d01bc088fde
LeGend077 <aadityaaarush78@gmail.com>
2026-01-01 21:45:48 +0530

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,58 @@
-import { useState, useEffect } from 'react';
-import './App.css'
-import Todo from './Todo';
+import { useState } from "react";
+import "./App.css";
+import Todo from "./Todo";
 
-function App() { 
-  const [todos, setTodos] = useState([]);
+function App() {
+  const [todos, setTodos] = useState(() => {
+    const stored = localStorage.getItem("todos");
+    return stored ? JSON.parse(stored) : [];
+  });
   const [todo, setTodo] = useState("");
-
-    useEffect(() => {
-	if (localStorage.getItem("todos")) {
-	    setTodos(JSON.parse(localStorage.getItem("todos")))
-	} else {
-	    localStorage.setItem("todos", JSON.stringify(todos))
-	}
-    }, [])
 
   return (
     <div className="p-4">
       <div className="text-center text-2xl">TO-DO LIST</div>
       <div className="flex items-center w-full justify-center">
-	<input type="text" name="todo" value={todo ?? ''} onChange={(e) => setTodo(e.target.value)} className="border-1 p-2 rounded-lg border-solid" placeholder="Complete homework..." maxLength="36" />	
-      <button className="p-2 border-2 border-solid bg-gray-900 text-white rounded-lg hover:bg-gray-800 active:bg-gray-700" onClick={() => {
-	if (todo) {setTodos([...todos, todo]); localStorage.setItem('todos', JSON.stringify([...todos, todo])); setTodo("")} else { return }      }}>Add Todo</button>
-      </div> <br/>
+        <input
+          type="text"
+          name="todo"
+          value={todo ?? ""}
+          onChange={(e) => setTodo(e.target.value)}
+          className="border-1 p-2 rounded-lg border-solid"
+          placeholder="Complete homework..."
+          maxLength="36"
+        />
+        <button
+          className="p-2 border-2 border-solid bg-gray-900 text-white rounded-lg hover:bg-gray-800 active:bg-gray-700"
+          onClick={() => {
+            if (todo) {
+              setTodos([...todos, todo]);
+              localStorage.setItem("todos", JSON.stringify([...todos, todo]));
+              setTodo("");
+            } else {
+              return;
+            }
+          }}
+        >
+          Add Todo
+        </button>
+      </div>{" "}
+      <br />
       <div className="flex flex-col items-center m-auto w-[80vw] sm:w-120 justify-center">
-	{
-	    todos.map((todo, index) => {
-		return (<Todo key={index} content={todo} index={index} todos={todos} setTodos={setTodos} />)
-	    })
-	}
+        {todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              content={todo}
+              index={index}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,15 +1,23 @@
 function Todo({ index, content, todos, setTodos }) {
-    return (
-	<div className="m-0.5 rounded p-2 bg-slate-200 flex w-full border-slate-400 border-1 border-solid justify-between">
-	    <div style={{overflow: "hidden"}}> { content } </div>
-	    <div> <button className="hover:outline-1 hover:outline-slate-400 rounded hover:outline-solid px-1" onClick={() => {
-		let newTodos = Array.from(todos)
-		newTodos.splice(index, 1)
-		setTodos([...newTodos])
-		localStorage.setItem('todos', JSON.stringify([...newTodos]))
-	    }}>✔</button></div>
-	</div>
-    )
+  return (
+    <div className="m-0.5 rounded p-2 bg-slate-200 flex w-full border-slate-400 border-1 border-solid justify-between">
+      <div style={{ overflow: "hidden" }}> {content} </div>
+      <div>
+        {" "}
+        <button
+          className="hover:outline-1 hover:outline-slate-400 rounded hover:outline-solid px-1"
+          onClick={() => {
+            let newTodos = Array.from(todos);
+            newTodos.splice(index, 1);
+            setTodos([...newTodos]);
+            localStorage.setItem("todos", JSON.stringify([...newTodos]));
+          }}
+        >
+          ✔
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
